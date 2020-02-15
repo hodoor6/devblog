64e7b14f9f09094e153d868daf0e5345e6cf9293
hodoor6 <hodoor6@yandex.ua>
2020-02-15 14:10:34 +0200

    crud для создания тегов  базовый фунционал

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/admin', function () {
 Route::group(['prefix'=>'admin', 'namespace' => 'Admin'], function() {
     Route::get('/', 'DashboardController@index');
     Route::resource('/categories', 'CategoriesController');
-
+    Route::resource('/tags', 'TagsController');
 
 });
 
